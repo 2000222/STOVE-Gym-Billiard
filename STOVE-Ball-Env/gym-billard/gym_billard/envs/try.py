2adb1470ff04333dbe59386b703e6addf43b7e52
Gemsarah <36289745+2000222@users.noreply.github.com>
2020-05-13 01:05:56 +0800

    Update try.py

--- STOVE-Ball-Env/gym-billard/gym_billard/envs/try.py
+++ STOVE-Ball-Env/gym-billard/gym_billard/envs/try.py
@@ -47,6 +47,9 @@ env.generate_fitting_run(env.BillardsEnv)
 env_0 = env.BillardsEnv(n=3, r=1., m=1., hw=10, granularity=5, res=32, t=1.,init_v_factor=0, friction_coefficient=0., seed=None, sprites=False)
 task = env.AvoidanceTask(env_0, 4, greyscale=False, action_force=0.3)
 task.get_action_space()
+
+
+
 task.get_framebuffer_shape()
 task.resolve_action(6)
 task.resolve_action(8)
@@ -61,3 +64,40 @@ action = task.resolve_action(6,8)
 frame_buffer, state, r, done = task.step_frame_buffer(_action=6)
 print(frame_buffer.shape)
 frame_buffer, state, r, done = task.step_frame_buffer(_action=8)
+
+
+
+import envs as env
+env_2 = env.BillardsEnv(n=3, r=1., m=1., hw=10, granularity=5, res=32, t=1.,init_v_factor=0, friction_coefficient=0., seed=None, sprites=False)
+task = env.AvoidanceTask(env_2, 4, greyscale=False, action_force=0.3)
+from gym.envs.classic_control import rendering
+img,state,r,done = task.step(8)
+viewer = rendering.SimpleImageViewer()
+#viewer.__init__(display)
+img = zoom(img, [5, 5, 1], order=0)
+viewer.imshow(img)
+viewer.isopen
+viewer.close()
+import matplotlib.pyplot as plt
+plt.imshow(img)
+plt.show()
+exit()
+
+import pygame
+screen = pygame.display.set_mode((200,200))
+img = pygame.image.load(img)
+screen.blit(img,(0,0))
+
+screen_width = 600
+screen_height = 400
+viewer_2 = rendering.Viewer(screen_width, screen_height)
+viewer_2.render()
+import numpy as np
+xs = np.linspace(-1.2, 0.6, 100)
+ys = np.sin(3 * xs)*.45+.55
+scale = 600/1.8
+xys = list(zip((xs+1.2)*scale, ys*scale))
+track = rendering.make_polyline(xys)
+track.set_linewidth(4)
+viewer_2.add_geom(track)
+viewer_2.add_geom(img)
